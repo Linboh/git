563e97e25dfdc029709295519535bd2f873efdf3
lin <632162883@qq.com>
2022-07-28 22:49:59 +0800

    hos-fix2 commit

---
+++ src/main/java/demo.java
@@ -0,0 +1,9 @@
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("111222");
+        System.out.println("111222");
+        System.out.println("hot-fix");
+        System.out.println("master2");
+        System.out.println("hot-fix2");
+    }
+}
